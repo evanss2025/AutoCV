e57194397748335d11957516eabd242f6c2fed47
Sophia E <125570727+evanss2025@users.noreply.github.com>
2025-06-30 21:56:09 +0100

    deploy prep

--- backend/server.py
+++ backend/server.py
@@ -92,4 +92,4 @@ def download(type):
 
 if __name__ == '__main__':
     # run the app, debug=true only for development, remove in production
-    app.run(debug=True, port=8080)
+    app.run(port=8080)
