5be898b7aec45c67bedc19f30f4667fbed5ab51c
Sophia E <125570727+evanss2025@users.noreply.github.com>
2025-07-01 14:53:39 +0100

    forgot to change

--- backend/server.py
+++ backend/server.py
@@ -88,4 +88,4 @@ def download(type):
 
 if __name__ == '__main__':
     # run the app, debug=true only for development, remove in production
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
